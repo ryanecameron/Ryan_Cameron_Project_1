020ec658fa08d28a305717a6100c00ee0593934c
ryanecameron <rcameron@student.bridgew.edu>
2021-02-06 10:44:35 -0500

    Initital commit.

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+"""Ryan Cameron
+- Personal Notes... Disregard:
+    > There are 3203 pages.
+"""
+
 import requests
 import secrets
 
 
 def get_data(url: str):
     final_data = []
-    final_url = f"{url}&api_key={secrets.api_key}&page=8"
+    final_url = f"{url}&api_key={secrets.api_key}&"
     response = requests.get(final_url)
     if response.status_code != 200:
         print(response.text)
@@ -16,10 +21,12 @@ def get_data(url: str):
 
 
 def main():
-    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=id,school.state,school.name"
+    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=id,school.state,school.name,school.city"
+    #url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&"
     all_data = get_data(url)
     for school_data in all_data:
         print(school_data)
+
 
 
 if __name__ == '__main__':
