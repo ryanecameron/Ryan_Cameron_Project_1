fb3ee2ea71bc9a779713f2491c127077545b76d0
ryanecameron <rcameron@student.bridgew.edu>
2021-02-07 21:47:05 -0500

    Adjusted pages printed per iteration, and added line number

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import requests
+import json
 import secrets
 
 
 def get_data(url: str):
     final_data = []
     # There are 3203 pages of data.
-    # For loop iterates 32 times, printing 100 pages per iteration.
-    get_pages = f"{url}&api_key={secrets.api_key}"
-    get_response = requests.get(get_pages)
-
-    for pages in range(32):
-        final_url = f"{url}&api_key={secrets.api_key}&per_page=100, page={pages}"
+    # For loop iterates 161 times, printing 20 pages per iteration and 3 pages
+    # for the final iteration.
+    for pages in range(161):
+        final_url = f"{url}&api_key={secrets.api_key}&page={pages}"
         response = requests.get(final_url)
         if response.status_code != 200:
             print(response.text)
@@ -21,12 +20,15 @@ def get_data(url: str):
     return final_data
 
 
+
 def main():
     url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=id,school.state,school.name,school.city,2018.student.size,2017.student.size,2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line,2016.repayment.3_yr_repayment.overall"
     all_data = get_data(url)
+    line_counter = 1
+    # For loop returns results by line, with line number
     for school_data in all_data:
-        print(school_data)
-
+        print(line_counter, ": ", school_data)
+        line_counter += 1
 
 
 if __name__ == '__main__':
