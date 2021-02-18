2708cbcb4cee75144b6309c67c922dfbe124ac36
ryanecameron <rcameron@student.bridgew.edu>
2021-02-18 00:23:29 -0500

    Second Test is now functioning properly

--- tests/test_prod.py
+++ tests/test_prod.py
@@ -33,19 +33,17 @@ def test_create_empty_database():
 
 def test_table_create():
     results = main.get_data(
-        f"https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=id,"
-        f"school.state,school.name,school.city,2018.student.size,2017.student.size,"
-        f"2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line,"
-        f"2016.repayment.3_yr_repayment.overall&api_key={secrets.api_key}&per_page={1}&page={1000}")
+        f"https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields="
+        f"school.name&api_key={secrets.api_key}")
     conn, cursor = main.open_db("test_db.sqlite")
     main.setup_db(cursor)
     cursor.execute('''CREATE TABLE IF NOT EXISTS test_school(
         test TEXT
         );''')
-    cursor.execute('''INSERT INTO test_school VALUES(?)''', [results['school.name']])
-    cursor.execute('''SELECT test FROM test_school''')
+    cursor.execute('''INSERT INTO test_school VALUES(?)''', [results[1000]['school.name']])
+    cursor.execute('''SELECT * FROM test_school''')
     result = cursor.fetchone()
-    assert result == ('The test was successful',)
+    assert result == ('Olympic College',)
 
 
 
